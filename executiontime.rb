6461955214ee81926aae6eb70611074f54d29e6c
Philip Lowe <philip.lowe94@gmail.com>
2021-03-19 14:25:47 -0400

    Sub sum phase 1

--- executiontime.rb
+++ executiontime.rb
@@ -26,10 +26,50 @@ end
 
 def my_min_two(arr)
 
-    min = arr[0]
-    (1..arr.length)
+    min = arr.first # O(1)
+    (1...arr.length).each do |idx| # O(n)
+        ele = arr[idx] # O(1)
+        if ele < min   # O(1)
+            min = ele  # O(1)
+        end
+    end
+    min #O(1)
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-# p my_min_one(list)
+# Cancel out the constants =>
+# O(n)
+# Linear time
 
+
+# Largest Contiguous Sub-sum
+
+# You have an array of integers and you want to find the largest contiguous (together in sequence) sub-sum. 
+# Find the sums of all contiguous sub-arrays and return the max.
+
+# Phase I
+
+# Write a function that iterates through the array and finds all sub-arrays using nested loops. First make an array to hold all sub-arrays. Then find the sums of each sub-array and return the max.
+
+# Discuss the time complexity of this solution.
+
+def largest_contiguous_subsum_one(list)
+    arr = []
+
+    list.each_with_index do |ele, idx|
+        arr << [ele]
+
+        
+
+
+
+
+# list = [5, 3, -7]
+# largest_contiguous_subsum(list) # => 8
+
+# # possible sub-sums
+# [5]           # => 5
+# [5, 3]        # => 8 --> we want this one
+# [5, 3, -7]    # => 1
+# [3]           # => 3
+# [3, -7]       # => -4
+# [-7]          # => -7
