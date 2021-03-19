f30e820219504fed8d5e8f619d62b36e457b2158
Travis <tputnam8170@gmail.com>
2021-03-19 16:43:18 -0400

    Second anagram phase 2

--- Anagrams.rb
+++ Anagrams.rb
@@ -5,38 +5,37 @@
 
 def first_anagram?(str1, str2)
 
-    # anagrams = Array.new(str1.length) {Array.new([])}
-
-    # (str1.length).times do |i|
-    #     str1.each_char do |char|
-    #         anagrams[i] << char
-    #     end
-    # end
-    # anagrams
-
-    anagrams = []
-    split_str1 = str1.split("")
-
-    split_str1.each_with_index do |ele, idx|
-        (idx...split_str1.length).each do |i|
-            anagrams << split_str1[idx..i]
-        end
+    str1.split("").permutation.each do |perm|   # O(n!)
+        word = perm.join("")                    # O(1)
+        return true if word == str2             # O(1)
     end
-
-    anagrams
-
-    #permutations
-    # or do it ourself
+    false                                       # O(1)
 end
 
-p first_anagram?("cat", "tac")
-# p first_anagram?(str1, str2)
+ # O(n!) = n!
+
+# p first_anagram?("cat", "tac")
+# p first_anagram?("elvis", "lives")
+
+# Phase II:
+
+# Write a method #second_anagram? that iterates over the first string. For each
+#  letter in the first string, find the index of that letter in the second string
+#   (hint: use Array#find_index) and delete at that index. The two strings are
+#    anagrams if an index is found for every letter and the second string is empty
+#     at the end of the iteration.
+
+# Try varying the length of the input strings. What are the differences between
+ #first_anagram? and #second_anagram??
 
 
-"cat"  
-"cta"  
-"act"
-"atc"
-"tac"
-"tca"
+def second_anagram?(str1, str2)
+    str2_arr = str2.split 
+    str1.each_char do |char|
+        str2_arr.find_index(char)
+    end
+end
+
+
+
 
