f3d3c2fff1c4bdcd63cdce29c9c93b45e2b5b3a9
Travis <tputnam8170@gmail.com>
2021-03-19 14:02:33 -0400

    Phase 1 of my_min

--- executiontime.rb
+++ executiontime.rb
@@ -6,9 +6,30 @@
 # First, write a function that compares each element to every other element of the list. Return the element if all other elements in the array are larger.
 # What is the time complexity for this function?
 def my_min_one(arr)
-    prc = Proc.new { |a, b| a < b }
-    arr.each { |ele1| arr.all? { |ele2| prc.call(ele1, ele2)} }
+
+    # best case = O(n)
+    # worse case = O(n^2)
+    
+    arr.each do |ele1|    # O(n)    
+       if arr.all? { |ele2| ele1 <= ele2 } # O(n)
+            return ele1     # O(1)
+       end
+    end
+    false
+end
+
+# O(n) * O(n) = n^2
+
+# Phase II
+
+# Now rewrite the function to iterate through the list just once while keeping track of the minimum. What is the time complexity?
+
+def my_min_two(arr)
+
+    min = arr[0]
+    (1..arr.length)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min_one(list)
+# p my_min_one(list)
+
